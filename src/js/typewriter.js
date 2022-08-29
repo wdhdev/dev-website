529a53feea423585cc7e20443052b654e00e52b3
William Harrison <william@williamdavidharrison.com.au>
2022-08-29 13:28:59 +0800

    updates

--- src/js/typewriter.js
+++ src/js/typewriter.js
@@ -41,11 +41,11 @@ TxtType.prototype.tick = function() {
 
 window.onload = function() {
     var elements = document.getElementsByClassName("typewrite");
-    for (var i=0; i<elements.length; i++) {
+    for(var i=0; i<elements.length; i++) {
         var toRotate = elements[i].getAttribute("data-type");
         var period = elements[i].getAttribute("data-period");
         if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
+            new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
 
