6acb1decea682c7e6538ca1dd82c106bf4a3d57f
William Harrison <william@williamdavidharrison.com.au>
2022-10-02 13:06:59 +0800

    update

---
+++ src/js/contact-form.js
@@ -0,0 +1,16 @@
+document.getElementById("contact-form").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+    reset();
+
+    document.querySelector(".alert").style.display = "block";
+
+    setTimeout(function() {
+        document.querySelector(".alert").style.display = "none";
+    }, 3000)
+}
+
+function reset() {
+    document.getElementById("contact-form").reset();
+}
