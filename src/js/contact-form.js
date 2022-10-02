d49ee9546a0fca7e118134540db8ebe8f671842d
William Harrison <william@williamdavidharrison.com.au>
2022-10-02 13:48:44 +0800

    update

--- src/js/contact-form.js
+++ src/js/contact-form.js
@@ -1,15 +1,15 @@
-document.getElementById("contact").addEventListener("submit", submitForm);
+// document.getElementById("contact").addEventListener("submit", submitForm);
 
-function submitForm(e) {
-    reset();
+// function submitForm(e) {
+//     reset();
 
-    document.querySelector(".alert").style.display = "block";
+//     document.querySelector(".alert").style.display = "block";
 
-    setTimeout(function() {
-        document.querySelector(".alert").style.display = "none";
-    }, 3000)
-}
+//     setTimeout(function() {
+//         document.querySelector(".alert").style.display = "none";
+//     }, 3000)
+// }
 
-function reset() {
-    document.getElementById("contact").reset();
-}
+// function reset() {
+//     document.getElementById("contact").reset();
+// }
