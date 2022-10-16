acb8f70dce4b03553d2de313d6538e959b97326b
William Harrison <william@williamdavidharrison.com.au>
2022-10-16 16:47:47 +0800

    css dark mode

--- src/js/theme.js
+++
@@ -1,7 +0,0 @@
-if(window.matchMedia) {
-  if(window.matchMedia("(prefers-color-scheme: dark)").matches){
-    $("#about").toggleClass("dark");
-    $("#projects").toggleClass("dark");
-    $("#social").toggleClass("dark");
-  }
-}
