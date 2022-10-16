cd83e11c3388cc2118bb01cccab833c1bd4b696d
William Harrison <william@williamdavidharrison.com.au>
2022-10-16 10:59:28 +0800

    Update theme.js

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,5 +1,8 @@
 if(window.matchMedia) {
   if(window.matchMedia('(prefers-color-scheme: dark)').matches){
     // Dark Mode
+    $("#about").addClass("dark");
+    $("#projects").addClass("dark");
+    $("#social").addClass("dark");
   }
 }
