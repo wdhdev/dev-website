98188e8e67454d8df8452af1d70a0f4ec730f2a0
William Harrison <william@williamdavidharrison.com.au>
2023-02-11 17:15:01 +0800

    add discord status + remove spin about pic

---
+++ src/js/discord-status.js
@@ -0,0 +1,18 @@
+fetch("https://api.lanyard.rest/v1/users/853158265466257448")
+    .then((res) => res.json())
+    .then((res) => {
+        switch (res.data.discord_status) {
+            case "online":
+                document.getElementById("about-pic").style.border = "4px solid #46A55F";
+                break;
+            case "idle":
+                document.getElementById("about-pic").style.border = "4px solid #F5A626";
+                break;
+            case "dnd":
+                document.getElementById("about-pic").style.border = "4px solid #E74044";
+                break;
+            case "offline":
+                document.getElementById("about-pic").style.border = "4px solid #757F8D";
+                break;
+        }
+    })
